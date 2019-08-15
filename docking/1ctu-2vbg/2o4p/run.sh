e2c7bda8cc779676eb8928d13511adf4b7c5b71e
siegel-lab-ucd <siegel.lab.ucd@gmail.com>
2019-08-15 16:05:57 -0700

    Add files via upload

---
+++ docking/1ctu-2vbg/2o4p/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/path/to/Rosetta/main/source/bin/rosetta_scripts.default.linuxgccrelease -database /path/to/Rosetta/main/database @flags -overwrite -parser:protocol 2o4p_docking.xml -s model.pdb 
