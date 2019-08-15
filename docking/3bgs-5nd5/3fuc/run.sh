20bec8a590f4b5a41381ef112f21ebdfef0527ef
siegel-lab-ucd <siegel.lab.ucd@gmail.com>
2019-08-15 16:20:28 -0700

    Add files via upload

---
+++ docking/3bgs-5nd5/3fuc/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/path/to/Rosetta/main/source/bin/rosetta_scripts.default.linuxgccrelease -database /path/to/Rosetta/main/database @flags -overwrite -parser:protocol 3fuc_docking.xml -s model.pdb 
+
---
+++ docking/3bgs-5nd5/3mng/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/path/to/Rosetta/main/source/bin/rosetta_scripts.default.linuxgccrelease -database /path/to/Rosetta/main/database @flags -overwrite -parser:protocol 3mng_docking.xml -s model.pdb
