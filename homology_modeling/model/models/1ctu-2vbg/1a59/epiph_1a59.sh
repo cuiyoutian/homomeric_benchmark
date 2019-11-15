9c6bb00274751bdef5a84d7f01e1e6ebaf2db485
siegel-lab-ucd <siegel.lab.ucd@gmail.com>
2019-11-15 14:17:12 -0800

    Add files via upload

---
+++ homology_modeling/model/models/1ctu-2vbg/1a59/epiph_1a59.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/path/to/Rosetta/main/source/bin/rosetta_scripts.default.linuxgccrelease -database /path/to/Rosetta/main/database @flags -in:file:fasta 1a59.fasta -parser:protocol hybridize.xml
